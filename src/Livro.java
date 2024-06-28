c4893d24b8e8fb861e10cb2b926291d35e8d4f3e
Guilherme Vieira <guilhermevfelix06@gmail.com>
2024-06-28 12:24:48 -0300

    classe Livro feita

---
+++ src/Livro.java
@@ -0,0 +1,29 @@
+public class Livro {
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private Boolean disponivel;
+
+    public Livro(String titulo, String autor, String isbn, Boolean disponivel) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.disponivel = true;
+    }
+
+    public void emprestar() {
+        if (disponivel){
+            disponivel = false;
+        } else {
+            System.out.println("Livro já está emprestado.");
+        }
+    }
+
+    public void devolver() {
+        disponivel = true;
+    }
+
+    public String toString() {
+        return "Livro [título=" + titulo + ", autor=" + autor + ", isbn=" + isbn + ", disponivel=" + disponivel + "]";
+    }
+}
